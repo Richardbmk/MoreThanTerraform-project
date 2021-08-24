91714e9511be80f1dd4aa5285f9d7895cdbd64c4
Richardbmk <rdobmk@gmail.com>
2021-08-24 17:39:19 +0000

    26. The Splat Expression

--- terraform-docker/main.tf
+++ terraform-docker/main.tf
@@ -4,30 +4,48 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "2.15.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
   }
 }
 
 provider "docker" {}
+provider "random" {}
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
 }
 
+resource "random_string" "random" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
+}
+
 resource "docker_container" "nodered_container" {
-  name  = "nodered"
+  count = 2
+  name  = join("-", ["nodered", random_string.random[count.index].result])
   image = docker_image.nodered_image.latest
   ports {
     internal = 1880
-    external = 1880
+    # external = 1880
   }
 }
 
-output "IP-Address" {
-    value = docker_container.nodered_container.ip_address
-    description = "This is the IP addres of the node-red container"
+output "IP-Address-1" {
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
+  description = "This is the IP addres and external port of the node-red container"
 }
 
-output "container-name" {
-    value = docker_container.nodered_container.name
-    description = "The name of the node-red container"
+output "container-name-1" {
+  value       = docker_container.nodered_container[*].name
+  description = "The name of the node-red container"
+}
+
+output "IP-Address-2" {
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+  description = "This is the IP addres and external port of the node-red container"
 }
