a6cc16308df0c44bdbd1e88709a10bdac472a63b
Richardbmk <rdobmk@gmail.com>
2021-09-04 15:20:16 +0000

    EC2 instance creation

--- initial-setup/terraform-install.sh
+++ initial-setup/terraform-install.sh
@@ -2,12 +2,14 @@
 sudo apt-get update -y
 sudo apt-get install wget unzip -y
 echo -n "Enter terraform version URL yu want to install: "
-# link to find terraform version --->  https://releases.hashicorp.com/terraform/
 read VERSION_URL
+# link to find terraform version --->  https://releases.hashicorp.com/terraform/
 # sudo wget https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_linux_amd64.zip
 sudo wget $VERSION_URL
-sudo unzip terraform_0.14.0_linux_amd64.zip
+echo -n "Name of the unzip file: "
+read UNZIP_NAME
+sudo unzip $UNZIP_NAME
 sudo mv terraform /usr/local/bin/
-sudo rm -f terraform_0.14.0_linux_amd64.zip
+sudo rm -f $UNZIP_NAME
 echo "TERRAFORM VERSION IS:"
 echo $(terraform version)
